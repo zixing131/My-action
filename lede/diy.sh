05fc4ac0eae33ebd23feedfe99b6805b27bd7684
mt <cloudorzi@gmail.com>
2022-01-11 17:44:50 +0800

    Update diy.sh

--- lede/diy.sh
+++ lede/diy.sh
@@ -4,6 +4,7 @@
 ./scripts/feeds update -a
 
 rm -rf package/lean/luci-app-jd-dailybonus package/lean/luci-theme-argon
+rm -rf package/feeds/packages/smartdns
 mkdir -p package/custom && cd package/custom
 git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns.git
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
@@ -22,4 +23,4 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
 
 # Modify some default settings
-# curl -fsSL https://raw.githubusercontent.com/1orz/My-action/master/lede/zzz-default-settings >package/lean/default-settings/files/zzz-default-settings
+curl -fsSL https://raw.githubusercontent.com/1orz/My-action/master/lede/zzz-default-settings >package/lean/default-settings/files/zzz-default-settings
