f79f04efd8caf1a703cc0e77e49c17a766fc7ba1
zixing <1311817771@qq.com>
2022-01-19 09:20:51 +0800

    Update diy.sh

--- openwrt/diy.sh
+++ openwrt/diy.sh
@@ -14,5 +14,5 @@ cd -
 ./scripts/feeds install -a
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
